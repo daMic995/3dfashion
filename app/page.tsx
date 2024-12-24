8ec278cc248df4d686a86ebd0402488bd9f61c8b
Semire <108496857+daMic995@users.noreply.github.com>
2024-12-24 05:03:08 +0000

    Revert "Installed three.js and added a 3D model viewer to"
    This reverts commit a53adf6a3cba4835e4c535d21c0a8ba00d4b80f3.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import React, { useEffect, useState } from 'react'
 import integrations from './integrations';
 
-
 function index() {
   const [message, setMessage] = useState('Loading...');
 
